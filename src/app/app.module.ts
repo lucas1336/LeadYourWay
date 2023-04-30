fa586755c4dc9f17189d63bc8a19b454787952a7
U202110085 <u202110085@upc.edu.pe>
2023-04-29 21:12:21 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { AboutComponent } from './components/home/about/about.component';
+import { TeamComponent } from './components/home/team/team.component';
+import { ContactComponent } from './components/home/contact/contact.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    TeamComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
