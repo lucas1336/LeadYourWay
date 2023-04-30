d346c2cd740e66c297ab4cd6993fd6148ec6b6c4
Oliver Tuesta <oliver.jtuesta@gmail.com>
2023-04-30 12:32:40 -0500

    feat: add routing to home

--- src/app/components/home/header/header.component.ts
+++ src/app/components/home/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  toggleSidenav(): void {
+    // Logic to toggle the side nav
+  }
 }
